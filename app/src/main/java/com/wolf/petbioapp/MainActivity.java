94ed0d607f3c7853b2fb07fccf722658f6b9c5a2
Wolfof420Street <billodida420@gmail.com>
2019-07-29 09:51:06 +0300

    designing initial Interface

--- app/src/main/java/com/wolf/petbioapp/MainActivity.java
+++ app/src/main/java/com/wolf/petbioapp/MainActivity.java
@@ -1,14 +1,39 @@
-package com.wolf.petbioapp;
+  package com.wolf.petbioapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+  public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView catImage;
+    private ImageView dogImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        catImage = (ImageView) findViewById(R.id.catImage);
+        dogImage = (ImageView) findViewById(R.id.dogImage);
+
+        catImage.setOnClickListener(this);
+        dogImage.setOnClickListener(this);
     }
-}
+
+      @Override
+      public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.catImage:
+                Toast.makeText(MainActivity.this, "You touched the Pussy", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.dogImage:
+                Toast.makeText(MainActivity.this, "You touched the Doggy", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+      }
+  }
